e328d40dfbe29f141ef9c10076474678cb96ec0f
Artemii <artemmac00@inbox.ru>
2026-06-24 15:32:52 +0300

    Update app.py

--- TaskManager/backend/app.py
+++ TaskManager/backend/app.py
@@ -101,7 +101,7 @@ def get_stats_route():
 
 if __name__ == '__main__':
     init_db()
-    host = os.environ.get('HOST', '127.0.0.1')
+    host = os.environ.get('HOST', '0.0.0.0')
     port = int(os.environ.get('PORT', 5000))
     debug = os.environ.get('DEBUG', 'True').lower() == 'true'
     app.run(host=host, port=port, debug=debug)
